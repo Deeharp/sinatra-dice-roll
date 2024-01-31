cc1348e8a6df7c6cc26cd388117622a19fd43bba
Deeharp <143421240+Deeharp@users.noreply.github.com>
2024-01-31 14:52:06 +0000

    finished website

--- dice.rb
+++ dice.rb
@@ -2,9 +2,16 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -23,5 +30,40 @@ get("/dice/2/6") do
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
   "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  sum = first_die 
+	
+  outcome = "You rolled a #{first_die} for a total of #{sum}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+	
+  "<h1>1d20</h1>
    <p>#{outcome}</p>"
 end
